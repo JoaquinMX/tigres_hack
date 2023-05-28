0c7096ac79da3fb2ad2fc9aba062d473c024931f
AnaVea19 <77868953+AnaVea19@users.noreply.github.com>
2023-05-28 00:52:37 -0600

    Components 1 iteration ready

---
+++ src/components/EmergencyButton/EmergencyButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './EmergencyButton.css';
+
+function EmergencyButton() {
+    return (
+      <button className='button'>
+        BOTÓN DE EMERGENCIA
+      </button>
+    );
+  }
+
+export default EmergencyButton;
